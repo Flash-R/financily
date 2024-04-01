ca6b1d3fd4ff7e54dfe04a48d047ffb188fd5258
Tumwesigye Ronnie <tumronald1@gmail.com>
2024-04-01 22:36:10 +0300

    Debuging the addcome file

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Cards from '../Components/Cards'
 import AddExpense from '../Components/Modal/AddExpense';
-import AddIncome from '../Components/Modal/AddIncome';
+import AddIncome from '../Components/Modal/addIncome';
 import { toast } from 'react-toastify';
 import { auth , db } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth'
