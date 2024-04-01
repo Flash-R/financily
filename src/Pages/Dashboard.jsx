491cc0ed74e6680ed7adfc8b9b739abd43b06ceb
Tumwesigye Ronnie <tumronald1@gmail.com>
2024-04-01 22:48:53 +0300

    Added the extension to the import

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Cards from '../Components/Cards'
 import AddExpense from '../Components/Modal/AddExpense';
-import AddIncome from '../Components/Modal/addIncome';
+import AddIncome from '../Components/Modal/addIncome.jsx';
 import { toast } from 'react-toastify';
 import { auth , db } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth'
